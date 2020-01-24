32f631fcfb5eb8e5be71fcb8019a1821d9b0f1a5
Cindy Okino <cindy.okino@hotmail.com>
2020-01-24 16:20:06 -0500

    add mongoDB atlas DataBase

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true });
+// LOCAL DATABASE
+// mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// MONGO DB ATLAS DATABASE
+mongoose.connect("mongodb+srv://nami:123@yelpcamp-t551a.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -48,6 +52,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
-	console.log("The YelpCamp Server Has Started!");
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("YelpCamp has started");
 });
